d5284252354efdf1037d8c21f4f64249befb814c
suilulu <m13869841793@gmail.com>
2016-12-24 08:10:49 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :group_relationships
-  validates :members, through: :group_relationships, source: :user
+  has_many :members, through: :group_relationships, source: :user
 end
