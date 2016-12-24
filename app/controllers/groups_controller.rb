88e3196b5cf01262c491ba96b1d0d8c13eaf2e0c
suilulu <m13869841793@gmail.com>
2016-12-24 13:09:23 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,10 +22,12 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
     end
+    
   end
 
   def update
@@ -50,7 +52,7 @@ class GroupsController < ApplicationController
     else
       flash[:warning] = "你已经是本讨论版成员了！"
     end
-    
+
     redirect_to group_path(@group)
   end
 
